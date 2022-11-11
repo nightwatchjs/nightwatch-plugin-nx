ed70b7d5038a9aab55631f3fd5a23e2d3079f185
AutomatedTester <david.burns@theautomatedtester.co.uk>
2022-11-11 22:11:24 +0000

    bump Nightwatch version support to 2.5.0

--- packages/nightwatch/src/utils/versions.ts
+++ packages/nightwatch/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = require('../../package.json').version;
 export const typesNodeVersion = '16.11.7';
-export const nightwatchVersion = '^2.4.0';
+export const nightwatchVersion = '^2.5.0';
 export const typesNightwatchVersion = '2.3.12';
 export const geckodriver = "3.2.0";
 export const chromedriver = "107.0.3";
